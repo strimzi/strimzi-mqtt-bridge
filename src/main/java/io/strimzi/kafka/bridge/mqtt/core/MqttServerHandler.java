1b90747c1fb9ebebca5c2827b3f306cc561f9f0b
Antonio Pedro <tonio.pedro99@gmail.com>
2023-06-09 17:15:40 +0530

    add: add copyright to all files. Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>

--- src/main/java/io/strimzi/kafka/bridge/mqtt/core/MqttServerHandler.java
+++ src/main/java/io/strimzi/kafka/bridge/mqtt/core/MqttServerHandler.java
@@ -1,5 +1,8 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
 package io.strimzi.kafka.bridge.mqtt.core;
-
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
