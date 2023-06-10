ceded50ebaaf640320a15cf9cc46471f0efb065b
António Pedro <42675180+antonio-pedro99@users.noreply.github.com>
2023-06-10 20:02:00 +0530

    Setup a basic Netty MQTT server (#3)
    * server basic setup
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * Basic Netty MQTT server setup
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: Rectory project structure
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: add .gitignore and delete IDE specific files.
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * fix: fix Netty version in pom.xml file.
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: Changed the package name, and JDK compiler version to 17, and the groudid and artifactid.
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * fix: import in MqttServerHandler
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: Improve .gitignore
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * remove .idea folder
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * fix: fix .gitignore and pom.xml
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: Add java doc for all defined classes and methods.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: Replace generic type in MqttServerHandler from Object to MqttMessage.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: Improve mqtt message type condition and remove boilerplate code.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: add logging
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: Rectory project folder structure
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: Rectory project folder structure
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: add missing logging in Main, MappingRunnable and MqttServer
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: reformat code.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: python script tester.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: python script tester.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: python script tester.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: add python script to test the server
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * del: delete python script tester.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * del: delete MqttKafkaMapper.java
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * del: delete MqttKafkaMapper.java
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * improve: Improve javadoc in handlePublishMessage method.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * del: delete util MappingRunnable.java class.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * ref: Reformat code.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * add: add copyright to all files.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    * imp: improve logging messages.
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>
    
    ---------
    
    Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>

---
+++ src/main/java/io/strimzi/kafka/bridge/mqtt/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.bridge.mqtt;
+
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.strimzi.kafka.bridge.mqtt.core.MqttServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    static final int PORT = Integer.parseInt(System.getProperty("port", "1883"));
+
+    public static void main(String[] args) {
+        try {
+            NioEventLoopGroup masterGroup = new NioEventLoopGroup();
+            NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+            MqttServer server = new MqttServer(PORT, masterGroup, workerGroup, ChannelOption.SO_KEEPALIVE);
+            server.start();
+        } catch (Exception e) {
+            logger.error("Error starting the MQTT server: ", e);
+        }
+    }
+}
---
+++ src/main/java/io/strimzi/kafka/bridge/mqtt/core/MqttServerHandler.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.bridge.mqtt.core;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.codec.mqtt.MqttMessageBuilders;
+import io.netty.handler.codec.mqtt.MqttMessageType;
+import io.netty.handler.codec.mqtt.MqttPublishMessage;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+import io.netty.handler.codec.mqtt.MqttConnAckMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.nio.charset.Charset;
+
+/**
+ * Represents a SimpleChannelInboundHandler. The MqttServerHandler is responsible for: <br>
+ * - listen to client connections;<br>
+ * - listen to incoming messages; <br>
+ *
+ * @see io.netty.channel.SimpleChannelInboundHandler
+ */
+public class MqttServerHandler extends SimpleChannelInboundHandler<MqttMessage> {
+    private static final Logger logger = LoggerFactory.getLogger(MqttServerHandler.class);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        logger.info("Client  {} is trying to connect", ctx.channel().remoteAddress());
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MqttMessage msg) {
+        try {
+            MqttMessageType messageType = msg.fixedHeader().messageType();
+            logger.debug("Got {} message type", messageType.name());
+            if (messageType == MqttMessageType.CONNECT) {
+                handleConnectMessage(ctx);
+            } else if (messageType == MqttMessageType.PUBLISH) {
+                MqttPublishMessage message = (MqttPublishMessage) msg;
+                message.retain();
+                handlePublishMessage(ctx, message);
+                message.release();
+            }
+        } catch (Exception e) {
+            logger.error("Error reading Channel: ", e);
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    /**
+     * Handle the case when a client sent a MQTT CONNECT message type.
+     *
+     * @param ctx ChannelHandlerContext instance
+     */
+    private void handleConnectMessage(ChannelHandlerContext ctx) {
+        MqttConnAckMessage connAckMessage = MqttMessageBuilders.connAck()
+                .sessionPresent(false)
+                .returnCode(MqttConnectReturnCode.CONNECTION_ACCEPTED)
+                .build();
+
+        logger.info("{} client connected.", ctx.channel().remoteAddress());
+        ctx.writeAndFlush(connAckMessage);
+    }
+
+    /**
+     * Handle the case when a client sent a MQTT PUBLISH message type.
+     *
+     * @param ctx            ChannelHandlerContext instance
+     * @param publishMessage represents a MqttPublishMessage
+     */
+    private void handlePublishMessage(ChannelHandlerContext ctx, MqttPublishMessage publishMessage) {
+        logger.info("Topic: {}", publishMessage.variableHeader().topicName());
+        logger.info("Message: {}", publishMessage.payload().toString(Charset.defaultCharset()));
+    }
+}
