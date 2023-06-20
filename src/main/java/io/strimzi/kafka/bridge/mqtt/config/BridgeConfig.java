79ccf4cb83cff22a08c7b2921e9b29cefeb0156f
Antonio Pedro <tonio.pedro99@gmail.com>
2023-06-20 15:05:55 +0530

    start config Signed-off-by: Antonio Pedro <tonio.pedro99@gmail.com>

---
+++ src/main/java/io/strimzi/kafka/bridge/mqtt/config/BridgeConfig.java
@@ -0,0 +1,8 @@
+package io.strimzi.kafka.bridge.mqtt.config;
+
+import java.util.Map;
+public class BridgeConfig extends AbstractConfig {
+    public BridgeConfig(Map<String, Object> config) {
+        super(config);
+    }
+}
