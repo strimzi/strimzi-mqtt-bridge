acd7bf8b33b082e1866d55d30a7d36b2c6011a3a
Paolo Patierno <ppatierno@live.com>
2023-06-26 18:18:17 +0200

    Refactoring from utils to mapper package (#9)
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- src/main/java/io/strimzi/kafka/bridge/mqtt/mapper/MqttKafkaMapper.java
+++ src/main/java/io/strimzi/kafka/bridge/mqtt/mapper/MqttKafkaMapper.java
@@ -2,9 +2,7 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.strimzi.kafka.bridge.mqtt.core;
-
-import io.strimzi.kafka.bridge.mqtt.utils.MappingRule;
+package io.strimzi.kafka.bridge.mqtt.mapper;
 
 import java.util.List;
 import java.util.ArrayList;
